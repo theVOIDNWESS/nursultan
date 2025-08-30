da5e74be53f1c89dc25ef873f6cdd3f9f96fc5a0
theVOIDNWESS <cyp06@westknitwearltd.com>
2025-08-30 16:51:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
-    #!/bin/bash
+#!/bin/bash
 
-    if [[ $EUID -ne 0 ]]; then
-      echo "Этот скрипт должен быть запущен от имени root (используйте sudo install.sh)."
-      exit 1
-    fi
+if [[ $EUID -ne 0 ]]; then
+  echo "Этот скрипт должен быть запущен от имени root (используйте sudo ./install.sh)."
+  exit 1
+fi
 
-    cp nursultan.py /usr/local/bin/nursultan
-    cp nursultan.py /usr/local/bin/nursultan.py
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-    chmod +x /usr/local/bin/nursultan
+echo "#!/usr/bin/env python3" | sudo tee /usr/local/bin/nursultan > /dev/null
+sudo cat "$SCRIPT_DIR/nursultan.py" | sudo tee -a /usr/local/bin/nursultan > /dev/null
 
-    sudo apt update  
+sudo chmod +x /usr/local/bin/nursultan
+
+if ! command -v pip3 &> /dev/null
+then
+    echo "Установка python3-pip..."
+    sudo apt update
     sudo apt install -y python3-pip
-    pip3 install -r requirements.txt --break-system-packages
-    
-    echo "Отправьте в консоль: nursultan"
+fi
+sudo pip3 install -r "$SCRIPT_DIR/requirements.txt" --break-system-packages
+
+echo "Скрипт nursultan успешно установлен.  Запустите его, набрав 'nursultan' в терминале."
 
